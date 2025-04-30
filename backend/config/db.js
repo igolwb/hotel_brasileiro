0955aef92c52c741dc8ef265d47ed548981e8cda
@_Ingoat <igolwb@gmail.com>
2025-04-29 23:50:05 -0300

    Add files via upload

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,3 +1,8 @@
+console.log('DB_NAME:', process.env.DB_NAME);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_HOST:', process.env.DB_HOST);
+
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
