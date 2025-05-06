e18697fcda0a8322d69339c3fffb68e04942efb3
@_Ingoat <igolwb@gmail.com>
2025-05-06 00:57:32 -0300

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,45 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-require('dotenv').config();
-const db = require('./config/db');
+import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+import dotenv from 'dotenv';
 
-// Import routes
-const authRoutes = require('./routes/authRoutes');
-const roomRoutes = require('./routes/roomRoutes');
-const reservationRoutes = require('./routes/reservationRoutes');
+import produtoRoutes from './routes/produtoRoutes.js';      // Importa as rotas de produtos do arquivo produtoRoutes.js
+import { sql } from '../config/db.js';                      // Importa a configuração do banco de dados do arquivo db.js
+
+dotenv.config();                                            // Carrega as variáveis de ambiente do arquivo .env para process.env
+                                                            // dotenv é um módulo que carrega variáveis de ambiente de um arquivo .env para process.env, permitindo o uso de variáveis de configuração em seu aplicativo.
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
-// Middleware
-app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());                                    // middleware para analisar o body das requisições JSON
+app.use(cors());                                            // middleware para habilitar CORS (Cross-Origin Resource Sharing), permitindo que o frontend acesse o backend em diferentes domínios.
+app.use(helmet());                                          // helmet é um middleware de segurança que ajuda a proteger o seu app definindo vários cabeçalhos HTTP relacionados à segurança.
+app.use(morgan('dev'));                                     // morgan é um middleware de logging que registra as requisições HTTP no console.
 
-// Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/rooms', roomRoutes);
-app.use('/api/reservations', reservationRoutes);
+app.use('/api/produtos', produtoRoutes);                                                         // Rota de teste que responde com "Hello World!" quando acessada.
 
-// Start server
-app.listen(PORT, async () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+async function startdb() {
   try {
-    await db.sync(); // Sync database
-    console.log('Database connected and synced');
-  } catch (err) {
-    console.error('Error syncing database:', err.message);
+    await sql`
+    CREATE TABLE IF NOT EXISTS produtos (
+      id SERIAL PRIMARY KEY,
+      nome VARCHAR(255) NOT NULL,
+      imagem VARCHAR(255) NOT NULL,
+      preco DECIMAL(10, 2) NOT NULL,
+      criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )
+    `;
+
+    console.log('db conectada');
+  }catch (error) {
+    console.error('Erro ao conectar ao banco de dados:', error); // Loga o erro caso a conexão falhe
   }
-});
+}
+
+startdb().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`); // Loga a porta em que o servidor está rodando
+  });
+})
