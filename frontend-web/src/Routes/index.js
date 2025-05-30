e59dd44bbe7db07034cac672c724d0375288ad8d
@_Ingoat <igolwb@gmail.com>
2025-05-30 17:01:01 -0300

    CRUD Clientes e Quartos completo

--- frontend-web/src/Routes/index.js
+++ frontend-web/src/Routes/index.js
@@ -1,10 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
-
+//<Route path="/admin/quartos/:quartoId" element={<Quartos />} />
 import HomePage from '../components/HomePage/homePage.js';
 import LoginPage from '../components/LoginPage/loginPage.js';
 import CadastroPage from '../components/CadastroPage/cadastro.js';
 import ReservaPage from '../components/ReservaPage/reservaPage.js';
 import Clientes from '../components/AdminPages/ClientePage/clientePage.js';
+import Quartos from '../components/AdminPages/QuartoPage/quartoPage.js';
+import EditarQuarto from '../components/AdminPages/EditarQuarto/editQuarto.js';
+import AddQuarto from '../components/AdminPages/EditarQuarto/addQuarto.js';
 
 
 function AppRoutes() {
@@ -15,6 +18,9 @@ function AppRoutes() {
       <Route path="/cadastro" element={<CadastroPage />} />
       <Route path="/reserva/:roomId" element={<ReservaPage />} />
       <Route path="/admin/clientes" element={<Clientes />} />
+      <Route path="/admin/quartos" element={<Quartos />} />
+      <Route path="/admin/quartos/:id" element={<EditarQuarto />} />
+      <Route path="/admin/quartos/addQuarto" element={<AddQuarto />} />
     </Routes>
   );
 }
