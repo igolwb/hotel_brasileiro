5a4dd66cf7c6551df80b86ef920c0fb1e980d174
@_Ingoat <igolwb@gmail.com>
2025-06-04 02:39:43 -0300

    CRUD completo

--- frontend-web/src/Routes/index.js
+++ frontend-web/src/Routes/index.js
@@ -1,5 +1,4 @@
 import { Routes, Route } from 'react-router-dom';
-//<Route path="/admin/quartos/:quartoId" element={<Quartos />} />
 import HomePage from '../components/HomePage/homePage.js';
 import LoginPage from '../components/LoginPage/loginPage.js';
 import CadastroPage from '../components/CadastroPage/cadastro.js';
@@ -8,6 +7,9 @@ import Clientes from '../components/AdminPages/ClientePage/clientePage.js';
 import Quartos from '../components/AdminPages/QuartoPage/quartoPage.js';
 import EditarQuarto from '../components/AdminPages/EditarQuarto/editQuarto.js';
 import AddQuarto from '../components/AdminPages/EditarQuarto/addQuarto.js';
+import AdminReservas from '../components/AdminPages/ReservasPage/reservasPage.js';
+import EditarReserva from '../components/AdminPages/EditarReserva/editReserva.js';
+import AddReserva from '../components/AdminPages/EditarReserva/addReserva.js';
 
 
 function AppRoutes() {
@@ -17,10 +19,16 @@ function AppRoutes() {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/cadastro" element={<CadastroPage />} />
       <Route path="/reserva/:roomId" element={<ReservaPage />} />
+      
       <Route path="/admin/clientes" element={<Clientes />} />
+
       <Route path="/admin/quartos" element={<Quartos />} />
       <Route path="/admin/quartos/:id" element={<EditarQuarto />} />
       <Route path="/admin/quartos/addQuarto" element={<AddQuarto />} />
+
+      <Route path="/admin/reservas" element={<AdminReservas />} />
+      <Route path="/admin/reservas/:id" element={<EditarReserva />} />
+      <Route path="/admin/reservas/addReserva" element={<AddReserva />} />
     </Routes>
   );
 }
